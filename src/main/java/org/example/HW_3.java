259beab63c1b109273aa43993340ffd6065f3d09
Slavbor <119975926+Slavbor@users.noreply.github.com>
2023-04-27 01:41:52 +0300

    code review

--- src/main/java/org/example/HW_3.java
+++ src/main/java/org/example/HW_3.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class HW_3 {
 
     public static void main(String[] args) {
-//1 Реализовать алгоритм обратной сортировки списков компаратором.
+
         System.out.println("1. Реализовать алгоритм обратной сортировки списков компаратором.");
         ArrayList<Integer> list_to_sort = new ArrayList<>();
         for (int i = 0; i < 40; i++) {
@@ -17,7 +17,6 @@ public class HW_3 {
         System.out.println("Отсортированый список: " + list_to_sort);
 
 
-//2   Пусть дан произвольный список целых чисел, удалить из него чётные числа
         System.out.println("\n" + "2 Пусть дан произвольный список целых чисел, удалить из него чётные числа");
         ArrayList<Integer> first_list = new ArrayList<>();
         for (int i = 0; i < 40; i++) {
@@ -28,7 +27,6 @@ public class HW_3 {
         first_list.removeIf(n -> n % 2 == 0);
         System.out.println("Отфильтрованый список: " + first_list);
 
-//3 Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее из этого списка.
         System.out.println("\n" + "3. Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее из этого списка.");
         int min = first_list.get(0);
         int max = min;
@@ -45,7 +43,6 @@ public class HW_3 {
         System.out.println("Максимум: " + max);
         System.out.println("Cреднее: " + average);
 
-//4 Дано два целочисленных списка, объеденить их не допуская элементы второго списка уже встречающиеся в первом.
         System.out.println("\n" + "4 Дано два целочисленных списка, объеденить их не допуская элементы второго списка уже встречающиеся в первом.");
         ArrayList<Integer> second_list = new ArrayList<>();
         List<Integer> crosslist = new ArrayList<Integer>(first_list);
@@ -60,8 +57,6 @@ public class HW_3 {
         first_list.addAll(second_list);
         System.out.println("Объединенный список: " + first_list);
 
-
-//5 Создать ArrayList <Integer> и добавить нулевым эллементом ноль 10000 раз.                 
         System.out.println("\n" + "5 Создать ArrayList <Integer> и добавить нулевым эллементом ноль 10000 раз.");
         long start = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
@@ -71,8 +66,7 @@ public class HW_3 {
         System.out.println(first_list);
         System.out.println("Время выполнения: " + (finish - start) + " миллисек.");
 
-//6 Повторить пятый пункт но с LinkedList.
-        System.out.println("\n" + "6 Повторить пятый пункт но с LinkedList");
+        System.out.println("\n" + "6 Повторить пятый пункт с LinkedList");
         LinkedList<Integer> LinkedList_array = new LinkedList<>();
         LinkedList_array.add(1);
         long begin_linked = System.currentTimeMillis();
